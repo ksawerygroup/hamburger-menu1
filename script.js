9d398377bf14bc3728be5c5796607bd7b442ddeb
Ksawery <ksawerygroup@gmail.com>
2018-09-14 21:48:35 +0200

    add file directory

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const burger = document.querySelector('.burger');
+const iconBurger = document.querySelector('.fa-bars');
+const iconX = document.querySelector('.fa-times');
+const aside = document.querySelector('aside');
 burger.addEventListener('click', function () {
-  burger.classList.toggle('active');
+  iconBurger.classList.toggle('show');
+  iconX.classList.toggle('show');
+  aside.classList.toggle('hide');
 })
