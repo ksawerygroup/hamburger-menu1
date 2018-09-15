291ed74069476c7a9a36bc45ae1811c78a6da387
Ksawery <ksawerygroup@gmail.com>
2018-09-15 07:04:14 +0200

    add file directory

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const burger = document.querySelector('.burger');
 const iconBurger = document.querySelector('.fa-bars');
 const iconX = document.querySelector('.fa-times');
-const aside = document.querySelector('aside');
+const column = document.querySelector('aside');
+const text = document.querySelector('.wrapper');
 burger.addEventListener('click', function () {
   iconBurger.classList.toggle('show');
   iconX.classList.toggle('show');
-  aside.classList.toggle('hide');
+  column.classList.toggle('hide');
+  text.classList.toggle('show');
 })
+
+//      JavaScript z pętlą
+
+// const burger = document.querySelector('.burger');
+// const activeElement = document.querySelectorAll('.active');
+// burger.addEventListener('click', function () {
+//   for (let i = 0; i < activeElement.length; i++) {
+//     activeElement[i].classList.toggle('show');
+//   }
+// })
+
+//      JQUery
+// $('.burger').on('click', function () {
+//   $('.fas, .wrapper').toggleClass('show'); //pobieranie dwóch elementów po przecinku
+//   $('aside').toggleClass('hide');
+// })
